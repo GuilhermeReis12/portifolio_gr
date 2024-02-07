e5c51874a2a86ada5c5245e49d6fbbf9ee891355
GuilhermeReis12 <guilhermeoliveira12102001@gmail.com>
2024-02-06 23:58:45 -0300

    start

---
+++ ui_protifolio/src/components/home/inicio.tsx
@@ -0,0 +1,19 @@
+// components/Header.js
+
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#">Link 1</a></li>
+          <li><a href="#">Link 2</a></li>
+          <li><a href="#">Link 3</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ ui_protifolio/src/pages/home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Inicio from '../components/home/inicio'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Bem-vindo à minha página inicial</h1>
+      <p>Esta é uma página inicial simples criada com Next.js</p>
+      <Inicio />
+    </div>
+  );
+};
+
+export default HomePage;
